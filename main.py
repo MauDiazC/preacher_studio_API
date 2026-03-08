703a679287ca5d769968af17f1b3ea72e270497a
maucho <mdiazcabr@gmail.com>
2026-03-07 18:00:53 -0600

    modifying main cors

--- main.py
+++ main.py
@@ -45,9 +45,10 @@ setup_logging()
 logger = logging.getLogger("fastapi")
 
 # Middleware de CORS
+allowed_origins = settings.get("ALLOWED_ORIGINS", "*").split(",")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
